3f07314957bf046404b6f0e01254f91e3d4cc81c
eKuG <ekanshgupta.eku@gmail.com>
2020-11-15 22:06:10 +0530

    Adds sum function

--- test.cpp
+++ test.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+int sum(int a, int b, int c, int d, int e){
+
+int x = a+b+c+d+e;
+return x;
+}
+
+
 int main() {
 
     int a = 3;
@@ -9,5 +16,6 @@ int main() {
     int c = 5;
     int d= 6 ;
     int e = 7;
-
+	int x = sum(a,b,c,d,e);
+	cout<<x;
 }
